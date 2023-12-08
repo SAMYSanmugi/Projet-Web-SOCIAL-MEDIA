329e9e12fd26c9e86951e372b984499738ac4330
IsabelleLIN <149782429+IsabelleLIN@users.noreply.github.com>
2023-12-08 14:53:16 +0100

    Update vous.js

--- vous.js
+++ vous.js
@@ -1,3 +1,4 @@
+//affichage quand on clique sur le bouton suivant 
 function afficherFormulaireDeux(){
 	// pour le 2e formulaire on fait "disparaitre" le premier 
 	document.getElementById("form1").style.display ="none"; 
@@ -23,12 +24,44 @@ function afficherFormulaireSix(){
 	document.getElementById("form5").style.display ="none"; 
 	document.getElementById("form6").style.display ="block"; 
 }
-		
+
 function afficherFormulaireSept(){
 	document.getElementById("form6").style.display ="none"; 
 	document.getElementById("form7").style.display ="block"; 
 }	
 
+//affichage quand on appuie sur le bouton precedent 
+function afficherPrecedentUn(){
+	document.getElementById("form2").style.display ="none"; 
+	document.getElementById("form1").style.display ="block"; 
+}
+
+function afficherPrecedentDeux(){
+	document.getElementById("form3").style.display ="none"; 
+	document.getElementById("form2").style.display ="block"; 
+}
+
+function afficherPrecedentTrois(){
+	document.getElementById("form4").style.display ="none"; 
+	document.getElementById("form3").style.display ="block"; 
+}
+
+function afficherPrecedentQuatre(){
+	document.getElementById("form5").style.display ="none"; 
+	document.getElementById("form4").style.display ="block"; 
+}
+
+function afficherPrecedentCinq(){
+	document.getElementById("form6").style.display ="none"; 
+	document.getElementById("form5").style.display ="block"; 
+}
+
+function afficherPrecedentSix(){
+	document.getElementById("form7").style.display ="none"; 
+	document.getElementById("form6").style.display ="block"; 
+}
+
+//fonction permettant d'ajouter des lignes 
 function ajouterCompetence() {
     var table = document.getElementById("competencesTable");
     var numeroCompetence = table.rows.length + 1; //afficher le num de la compétence
@@ -47,6 +80,18 @@ function ajouterLangues(){
     var cell2 = newRow.insertCell(1);
     cell1.innerHTML = "Langue" + ' ' + numeroLangue;
     cell2.innerHTML = '<input type="text" name="langue[]">';
+}
+
+//fonction permettant de charger le fichier XML 
+function loadXMLDoc(){
+	var xmlhttp = new XMLHttpRequest();
+	xmlhttp.onreadystatechange = function(){
+		if (this.readyState == 4 && this.status == 200){
+			genererCV(this); 
+		}
+	};
+	xmlhttp.open("GET", "vous.xml", true); 
+	xmlhttp.send(); 
 }
 
 function genererCV(){
