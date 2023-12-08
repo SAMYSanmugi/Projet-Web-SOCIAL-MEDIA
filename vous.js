e51890e3eb5c1495e43bb6b2c08b0c47e93ccbf2
IsabelleLIN <149782429+IsabelleLIN@users.noreply.github.com>
2023-12-08 13:24:54 +0100

    Update vous.js

--- vous.js
+++ vous.js
@@ -2,7 +2,7 @@ function afficherFormulaireDeux(){
 	// pour le 2e formulaire on fait "disparaitre" le premier 
 	document.getElementById("form1").style.display ="none"; 
 	document.getElementById("form2").style.display ="block"; 
-}
+} 
 
 function afficherFormulaireTrois(){
 	document.getElementById("form2").style.display ="none"; 
@@ -28,3 +28,27 @@ function afficherFormulaireSept(){
 	document.getElementById("form6").style.display ="none"; 
 	document.getElementById("form7").style.display ="block"; 
 }	
+
+function ajouterCompetence() {
+    var table = document.getElementById("competencesTable");
+    var numeroCompetence = table.rows.length + 1; //afficher le num de la compétence
+    var newRow = table.insertRow(table.rows.length);
+    var cell1 = newRow.insertCell(0);
+    var cell2 = newRow.insertCell(1);
+    cell1.innerHTML = "Compétence" + ' ' + numeroCompetence;
+    cell2.innerHTML = '<input type="text" name="competence[]">';
+}
+
+function ajouterLangues(){
+	var table = document.getElementById("languesTable");
+    var numeroLangue = table.rows.length + 1; 
+    var newRow = table.insertRow(table.rows.length);
+    var cell1 = newRow.insertCell(0);
+    var cell2 = newRow.insertCell(1);
+    cell1.innerHTML = "Langue" + ' ' + numeroLangue;
+    cell2.innerHTML = '<input type="text" name="langue[]">';
+}
+
+function genererCV(){
+
+}
