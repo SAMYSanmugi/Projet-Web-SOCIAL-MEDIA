6212754d1d535c65641ed1a528c4580408b8f14f
IsabelleLIN <149782429+IsabelleLIN@users.noreply.github.com>
2023-12-17 20:56:25 +0100

    Update vous.php

--- vous.php
+++ vous.php
@@ -13,7 +13,6 @@
    $exp = isset($_POST["experience"])? $_POST["experience"] : "";
    $forma = isset($_POST["formation"])? $_POST["formation"] : "";
    $erreur = "";
-
    
    if ($name == "") {
    $erreur .= "Le champ Nom est vide. <br>";
@@ -34,60 +33,53 @@
    if ($email == "") {
       $erreur .= "Le champ Adresse email est vide. <br>";
    }
+   if ($profile == "") {
+      $profile .= "Le champ Profile est vide. <br>";
+   }
+   if ($comp == "") {
+      $comp .= "Le champ Competence est vide. <br>";
+   }
+   if ($int == "") {
+      $int .= "Le champ Interet est vide. <br>";
+   }
+   if ($languages == "") {
+      $languages .= "Le champ Langues est vide. <br>";
+   }
+   if ($exp == "") {
+      $exp .= "Le champ Expérience est vide. <br>";
+   }
+   if ($forma == "") {
+      $forma .= "Le champ Formation est vide. <br>";
+   }
    if ($erreur == "") {
-      echo "Formulaire valide.";
+      echo "<td>$name</td>&nbsp&nbsp";
+      echo "<td>$fname</td><br><br>";
+
+      echo "Coordonnées :<br>";
+      echo "<td>Date de naissance :&nbsp</td><td>$birthday</td><br>";
+      echo "<td>Adresse :&nbsp</td><td>$postale</td><br>";
+      echo "<td>Téléphone :&npsp</td><td>$phone</td><br>";
+      echo "<td>Adresse-mail :&nbsp</td><td>$email</td><br><br><br>";
+
+      echo "Profil :";
+      echo "<td>$profile</td>";
+
+      echo "Compétences :";
+      echo "<td>$comp</td><br>";
+
+      echo "Centres d'intérêt :";
+      echo "<td>$int</td>";
+
+      echo "Langues :";
+      echo "<td>$languages</td>";
+
+      echo "Expériece :";
+      echo "<td>$exp</td>";
+
+      echo "Formation :";
+      echo "<td>$forma</td>";   
    } else {
       echo "Erreur: <br>" . $erreur;
    }
 
-   // Créer un objet SimpleXMLElement pour construire le fichier XML
-   $xml = new SimpleXMLElement('<?xml version="1.0" encoding="UTF-8"?><cv></cv>');
-
-   // Ajouter les éléments au fichier XML
-   $xml->addChild('nom', $name);
-   $xml->addChild('prenom', $fname);
-   $xml->addChild('naissance', $birthday);
-   $xml->addChild('adresse', $postale);
-   $xml->addChild('telephone', $phone);
-   $xml->addChild('mail', $email);
-   $xml->addChild('profil', $profile);
-   
-   // Traitement des champs qui peuvent avoir plusieurs entrées
-   if (!empty($comp)) {
-      $competences = $xml->addChild('competences');
-      foreach ($comp as $competence) {
-         $competences->addChild('competence', $competence);
-      }
-   }
-
-   $xml->addChild('interet', $int);
-
-   if (!empty($languages)) {
-      $langues = $xml->addChild('langues');
-      foreach ($languages as $langue) {
-         $langues->addChild('langue', $langue);
-      }
-   }
-
-   if (!empty($exp)) {
-      $experiences = $xml->addChild('experiences');
-      foreach ($exp as $experience) {
-         experiences->addChild('experience', $experience);
-      }
-   }
-
-   if (!empty($forma)) {
-      $formations = $xml->addChild('formations');
-      foreach ($forma as $formation) {
-         $formations->addChild('formation', $formation);
-      }
-   }
-
-
-   // Enregistrez le fichier XML
-   $xml->asXML('cv.xml');
-
-   // Rediriger vers une page de confirmation ou toute autre action souhaitée
-   header("Location: confirmation.html");
-   exit();
 ?>
